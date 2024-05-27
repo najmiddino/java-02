a15648c4a2371f3297517e24b639c81b50a0e783
najmiddino <164999098+najmiddino@users.noreply.github.com>
2024-05-27 22:59:27 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+let text = prompt('String turidagio malumot kiriting:')
+let num = +prompt('Number turidagi malumot kiriting:')
+let boolean = prompt('Boolean turiodagi malumot kiritinga:')
+
+confirm('Hamma kiritgan malumotlaringiz togriligiga ishonchingiz komilmi?')
+
+
+alert(`
+Siz kiritgan ${text} sozi ${typeof(text)} turida yozilgan
+Siz kiritgan ${num} sozi ${typeof(num)} turida yozilgan
+Siz kiritgan ${boolean} sozi ${typeof(boolean == boolean)} turida yozilgan
+`)
